6f6fc04269a1023cb6ee3e31c7ff87983391d102
Masker <1262917629@qq.com>
2022-01-23 14:49:40 +0800

    新建Netty入门模块

---
+++ introductory/src/test/java/com/wu/study/netty/introductory/hello/HelloServer.java
@@ -0,0 +1,13 @@
+package com.wu.study.netty.introductory.hello;
+
+/**
+ * @author Haixin Wu
+ * @date 2022/1/23 14:48
+ * @since 1.0
+ */
+public class HelloServer {
+    public static void main(String[] args) {
+
+    }
+}
+
