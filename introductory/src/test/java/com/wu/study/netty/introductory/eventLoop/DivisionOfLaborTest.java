e5cf2b39ff0ac9c429c119a699f5093854f8479a
Masker <1262917629@qq.com>
2022-01-25 22:36:02 +0800

    分工细化

---
+++ introductory/src/test/java/com/wu/study/netty/introductory/eventLoop/DivisionOfLaborTest.java
@@ -0,0 +1,51 @@
+package com.wu.study.netty.introductory.eventLoop;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.DefaultEventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+
+/**
+ * @author Haixin Wu
+ * @date 2022/1/25 22:13
+ * @since 1.0
+ */
+@Slf4j
+public class DivisionOfLaborTest {
+    public static void main(String[] args) {
+        // 细分2. 创建一个独立的 EventLoopGroup
+        DefaultEventLoopGroup group = new DefaultEventLoopGroup();
+        new ServerBootstrap()
+                //细分1.     ParentGroup 负责 accept    childrenGroup 负责 读写
+                .group(new NioEventLoopGroup(), new NioEventLoopGroup(2))
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast("handler1", new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf buf = (ByteBuf) msg;
+                                log.debug(buf.toString(Charset.defaultCharset()));
+                                ctx.fireChannelRead(msg);
+                            }
+                        }).addLast(group, "handler2", new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf buf = (ByteBuf) msg;
+                                log.debug(buf.toString(Charset.defaultCharset()));
+                            }
+                        });
+                    }
+                })
+                .bind(8080);
+    }
+}
