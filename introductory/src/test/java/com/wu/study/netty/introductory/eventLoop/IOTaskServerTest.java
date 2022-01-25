b7fe500fce170465cd3088126e120daebe4bf0df
Masker <1262917629@qq.com>
2022-01-25 22:11:54 +0800

    IO任务

---
+++ introductory/src/test/java/com/wu/study/netty/introductory/eventLoop/IOTaskServerTest.java
@@ -0,0 +1,44 @@
+package com.wu.study.netty.introductory.eventLoop;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+
+/**
+ * @author Haixin Wu
+ * @date 2022/1/25 21:53
+ * @since 1.0
+ */
+@Slf4j
+public class IOTaskServerTest {
+    public static void main(String[] args) {
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf buf = (ByteBuf) msg;
+                                log.debug(buf.toString(Charset.defaultCharset()));
+                                super.channelRead(ctx, msg);
+                            }
+                        });
+                    }
+                })
+                .bind(8080);
+
+    }
+}
+
---
+++ introductory/src/test/java/com/wu/study/netty/introductory/eventLoop/IOTestClientTest.java
@@ -0,0 +1,37 @@
+package com.wu.study.netty.introductory.eventLoop;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author Haixin Wu
+ * @date 2022/1/25 21:59
+ * @since 1.0
+ */
+@Slf4j
+public class IOTestClientTest {
+    public static void main(String[] args) throws InterruptedException {
+        Channel channel = new Bootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                .connect(new InetSocketAddress("localhost", 8080))
+                .sync()
+                .channel();
+        System.out.println(channel);
+        System.out.println("");
+    }
+}
+
