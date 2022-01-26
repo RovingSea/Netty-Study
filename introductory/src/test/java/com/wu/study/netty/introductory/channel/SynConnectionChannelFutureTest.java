32afd6b9a4a11f3f2199427eaf3a3cca22193029
Masker <1262917629@qq.com>
2022-01-26 15:48:53 +0800

    ChannelFuture同步连接

---
+++ introductory/src/test/java/com/wu/study/netty/introductory/channel/SynConnectionChannelFutureTest.java
@@ -0,0 +1,44 @@
+package com.wu.study.netty.introductory.channel;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+/**
+ * @author Haixin Wu
+ * @date 2022/1/26 0:18
+ * @since 1.0
+ */
+public class SynConnectionChannelFutureTest {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ChannelFuture channelFuture = new Bootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        socketChannel.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                // 该方法为异步非阻塞方法，主线程调用后不会被阻塞，真正去执行连接操作的是NIO线程
+                // NIO线程：NioEventLoop 中的线程
+                .connect(new InetSocketAddress("localhost", 8080));
+
+        // 该方法用于等待连接真正建立
+        channelFuture.sync();
+
+        // 获取客户端-服务器之间的Channel对象
+        Channel channel = channelFuture.channel();
+        channel.writeAndFlush("hello world");
+        System.in.read();
+    }
+}
+
