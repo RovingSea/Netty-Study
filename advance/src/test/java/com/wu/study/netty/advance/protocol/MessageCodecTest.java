935e5a869b2c328b15b8a4ff4fa1273c2a08497d
Masker <1262917629@qq.com>
2022-01-30 14:46:22 +0800

    update:【测试类】测试自定义解码器使用了@Shareable能否成功

--- advance/src/test/java/com/wu/study/netty/advance/protocol/MessageCodecTest.java
+++ advance/src/test/java/com/wu/study/netty/advance/protocol/MessageCodecTest.java
@@ -2,9 +2,9 @@ package com.wu.study.netty.advance.protocol;
 
 import com.wu.study.netty.advance.chat.LoginRequestMessage;
 import com.wu.study.netty.advance.protocol.div.MessageCodec;
+import com.wu.study.netty.advance.protocol.div.SharableMessageCodec;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
-import io.netty.channel.Channel;
 import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import io.netty.handler.logging.LogLevel;
@@ -19,27 +19,27 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MessageCodecTest {
     public static void main(String[] args) throws Exception {
-            EmbeddedChannel channel = new EmbeddedChannel();
-            // 添加解码器，避免粘包半包问题
+        EmbeddedChannel channel = new EmbeddedChannel();
+        // 添加解码器，避免粘包半包问题
         channel.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+        SharableMessageCodec sharableMessageCodec = new SharableMessageCodec();
         channel.pipeline().addLast(new LengthFieldBasedFrameDecoder(
-                    1024,
-                    12,
-                    4,
-                    0,
-                    0));
-            channel.pipeline().addLast(new MessageCodec());
-            LoginRequestMessage user = new LoginRequestMessage("Masker", "123");
+                1024,
+                12,
+                4,
+                0,
+                0));
+        LoginRequestMessage user = new LoginRequestMessage("Masker", "123");
+        channel.pipeline().addLast(sharableMessageCodec);
+        // 测试编码与解码
+        ByteBuf byteBuf = ByteBufAllocator.DEFAULT.buffer();
+        new MessageCodec().encode(null, user, byteBuf);
 
-            // 测试编码与解码
-            ByteBuf byteBuf = ByteBufAllocator.DEFAULT.buffer();
-            new MessageCodec().encode(null, user, byteBuf);
+        ByteBuf byteBuf1 = byteBuf.slice(0, 100);
+        ByteBuf byteBuf2 = byteBuf.slice(100, byteBuf.readableBytes() - 100);
 
-            ByteBuf byteBuf1 = byteBuf.slice(0, 100);
-            ByteBuf byteBuf2 = byteBuf.slice(100,  byteBuf.readableBytes() - 100);
-
-            byteBuf1.retain(); // 让引用计数 + 1
-            channel.writeInbound(byteBuf1); // 会调用 release 使至引用计数 - 1
-            channel.writeInbound(byteBuf2);
-        }
+        byteBuf1.retain(); // 让引用计数 + 1
+        channel.writeInbound(byteBuf1); // 会调用 release 使至引用计数 - 1
+        channel.writeInbound(byteBuf2);
+    }
 }
